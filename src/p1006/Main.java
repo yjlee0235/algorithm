37af68dd37c96dc1e49dc29f9d2babcc97d84044
yjlee0235 <yjlee0235@naver.com>
2022-10-12 20:55:04 +0900

    add: 10/12 문제풀이 (DP)

--- src/p1006/Main.java
+++ src/p1006/Main.java
@@ -1,20 +1,47 @@
-package basic;
+package p1006;
 
 import java.util.Scanner;
 
+class Problem {
+    public int score;
+    public int time;
+
+    public Problem(int score, int time) {
+        this.score = score;
+        this.time = time;
+    }
+}
+
 public class Main {
 
-    private String solution(String str) {
-        String answer = "";
+    static Problem[] problems;
+    static int[] dp;
 
-        return answer;
+    private int solution(int m) {
+        for (int i = 0; i < problems.length; i++) {
+            Problem tmp = problems[i];
+            for (int j = m; j >= tmp.time; j--) {
+                if (dp[j - tmp.time] + tmp.score > dp[j]) {
+                    dp[j] = dp[j - tmp.time] + tmp.score;
+                }
+            }
+        }
+        return dp[m];
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String input = kb.next();
+        int N = kb.nextInt();
+        int M = kb.nextInt();
+        problems = new Problem[N];
+        dp = new int[M + 1];
+        for (int i = 0; i < N; i++) {
+            int score = kb.nextInt();
+            int time = kb.nextInt();
+            problems[i] = new Problem(score, time);
+        }
 
-        System.out.println(T.solution(input));
+        System.out.println(T.solution(M));
     }
 }
