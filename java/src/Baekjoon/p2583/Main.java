3ec13ae3bcf4f61e8123562e82ac27e8c3f55d63
yjlee0235 <yjlee0235@naver.com>
2022-12-21 21:26:29 +0900

    refactor: 패키지 구조 변경

---
+++ java/src/Baekjoon/p2583/Main.java
@@ -0,0 +1,27 @@
+package Baekjoon.p2583;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int board[][];
+
+    public List solution() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String input = bf.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+
+
+        T.solution();
+    }
+}
